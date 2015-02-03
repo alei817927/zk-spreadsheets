941a6cf088aec0050d94db410bb6d92720aed24f
Yang Chunlei <yangchunlei@makenv.com>
2015-02-03 10:51:11 +0800

    干掉了lisence

--- zssex/src/org/zkoss/zssex/init/WebAppInit.java
+++ zssex/src/org/zkoss/zssex/init/WebAppInit.java
@@ -7,22 +7,12 @@ package org.zkoss.zssex.init;
 
 import org.zkoss.util.logging.Log;
 import org.zkoss.zk.ui.WebApp;
-import org.zkoss.zssex.rt.Runtime;
 
 public class WebAppInit implements org.zkoss.zk.ui.util.WebAppInit {
   public WebAppInit() {
   }
 
   public void init(WebApp var1) throws Exception {
-    if(!Runtime.init(var1, true)) {
-      String var2 = Runtime.WARNING_EVALUATION;
-      Log var3 = Log.lookup("global");
-      if(var3.errorable()) {
-        var3.error(var2);
-      } else {
-        System.err.println(var2);
-      }
-    }
 
   }
 }
